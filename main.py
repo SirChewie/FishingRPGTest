a8fc692f197a630f6594e157dfba8d2c64284872
EyeceScream <jhb1085@gmail.com>
2022-02-02 16:16:31 -0600

    StBar finished and Fish drawn.
    Added the stress bar and fish(Needs work)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from sys import exit
 
@@ -8,6 +9,7 @@ screen = pygame.display.set_mode((800, 400))
 # Title of main window
 pygame.display.set_caption("Fishing4Days")
 # Icon of main window
+pygame.display.set_icon(pygame.image.load('Imgs/Fish/GreenFins_YellowBelly.png').convert_alpha())
 clock = pygame.time.Clock()
 
 # Landscape
@@ -25,15 +27,19 @@ class Ui:
         self.sy = sy
         self.surf_skin = pygame.image.load('Imgs/UI/StressBarSkin.png').convert_alpha()
         self.rect_skin = self.surf_skin.get_rect(midbottom=(sx, sy))
+        self.stress_bar_R = pygame.image.load('Imgs/UI/StressBarColorR.png')
+        self.stress_bar_R_rect = self.stress_bar_R.get_rect(midbottom=(sx, sy))
         self.surf = pygame.image.load('Imgs/UI/StressBarLine.png').convert()
         self.rect = self.surf.get_rect(midbottom=(x, y))
         self.color = (120, 241, 56)
         self.velX = 0
         self.velY = 0
         self.speed = 3
+        self.x = True
 
     def draw(self, screen):
         if player.is_fishing:
+            screen.blit(self.stress_bar_R, self.stress_bar_R_rect)
             screen.blit(self.surf_skin, self.rect_skin)
             screen.blit(self.surf, self.rect)
 
@@ -41,12 +47,16 @@ class Ui:
         self.velX = 0
         self.velY = 0
 
+        # can fish or not
         if not player.is_fishing:
-            if player.mouseL_pressed:
-                print(str(player.mouse_pos) + "fish")
+            if player.space_pressed and player.mouse_pos[1] >= 315:
+                player.fishing_pos = (player.mouse_pos[0], player.mouse_pos[1])
                 player.is_fishing = True
 
         if player.is_fishing:
+            player.mobile = False
+            if (fish.x, fish.y) == (player.fishing_pos[0], player.fishing_pos[1]):
+                player.hooked = True
 
             if player.mouseL_pressed and not player.mouseM_pressed and not player.mouseR_pressed:
                 print("Reeling" + str(player.line_str))
@@ -54,7 +64,9 @@ class Ui:
                     if player.line_str <= 0:
                         print('Line broke')
                         ui.x = 369
+                        player.hooked = False
                         player.is_fishing = False
+                        player.mobile = True
                         player.line_str = 300
                         return
                     player.line_str -= 10
@@ -65,23 +77,49 @@ class Ui:
             elif player.line_str <= 0:
                 print('Line broke')
                 ui.x = 369
+                player.hooked = False
                 player.is_fishing = False
+                player.mobile = True
                 player.line_str = 300
                 return
             else:
                 if ui.x <= 250:
                     print('Fish got off')
                     ui.x = 369
+                    player.hooked = False
                     player.is_fishing = False
+                    player.mobile = True
                     player.line_str = 300
                     return
                 player.line_str += .5
                 self.velX -= self.speed / 2
+            self.x += self.velX
+            self.y += self.velY
+            self.stress_bar_R_rect = pygame.Rect(self.sx, self.sy, 210, 25)
+            self.rect = pygame.Rect(self.x, self.y, 7, 17)
+            self.rect_skin = pygame.Rect(self.sx, self.sy, 210, 25)
+
+
+class Fish:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.surf = pygame.image.load('Imgs/Fish/GreenFins_YellowBelly.png')
+        self.rect = self.surf.get_rect(midbottom=(x, y))
+        self.velX = 0
+        self.velY = 0
+        self.is_hooked = False
+
+    def draw(self, screen):
+        screen.blit(self.surf, self.rect)
+
+    def update(self):
+        self.velX = 0
+        self.velY = 0
 
         self.x += self.velX
         self.y += self.velY
-        self.rect = pygame.Rect(self.x, self.y, 7, 17)
-        self.rect_skin = pygame.Rect(self.sx, self.sy, 210, 25)
+        self.rect = pygame.Rect(self.x, self.y, 25, 11)
 
 
 class Player:
@@ -93,6 +131,7 @@ class Player:
         self.velX=0
         self.velY=0
         self.color = (120, 241, 56)
+        self.mobile = True
         self.mouse_pos = pygame.mouse.get_pos()
         self.mouseL_pressed = False
         self.mouseM_pressed = False
@@ -101,9 +140,12 @@ class Player:
         self.right_pressed = False
         self.up_pressed = False
         self.down_pressed = False
+        self.space_pressed = False
         self.speed = 1
         self.is_fishing = False
         self.line_str = 300
+        self.hooked = False
+        self.fishing_pos = (0, 0)
 
     def draw(self, screen):
         screen.blit(self.surf, self.rect)
@@ -119,8 +161,9 @@ class Player:
             self.velY = -self.speed
         if self.down_pressed and not self.up_pressed:
             self.velY = self.speed
+        if self.space_pressed:
+            self.is_fishing = True
 
-        # can fish or not
         self.x += self.velX
         self.y += self.velY
         self.rect = pygame.Rect(self.x, self.y, 32, 64)
@@ -128,7 +171,7 @@ class Player:
 
 player = Player(50, 100)
 ui = Ui(253, 369, 250, 365)
-
+fish = Fish(random.randint(0, 800), random.randint(315, 400))
 # Main game loop
 while True:
 
@@ -139,7 +182,7 @@ while True:
             exit()
 
         # Movement
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and player.mobile:
             if event.key == pygame.K_a:
                 player.left_pressed = True
             if event.key == pygame.K_d:
@@ -148,6 +191,8 @@ while True:
                 player.up_pressed = True
             if event.key == pygame.K_s:
                 player.down_pressed = True
+            if event.key == pygame.K_SPACE:
+                player.space_pressed = True
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 player.left_pressed = False
@@ -157,9 +202,11 @@ while True:
                 player.up_pressed = False
             if event.key == pygame.K_s:
                 player.down_pressed = False
+            if event.key == pygame.K_SPACE:
+                player.space_pressed = False
 
         # Mouse Controls
-
+        player.mouse_pos = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 player.mouseL_pressed = True
@@ -183,11 +230,13 @@ while True:
     screen.blit(tree_surf, (0, 25))
     player.draw(screen)
     ui.draw(screen)
-
+    fish.draw(screen)
     # update
     player.update()
     ui.update()
+    fish.update()
     pygame.display.flip()
+
     # tick/frame rate
     clock.tick(120)
 
